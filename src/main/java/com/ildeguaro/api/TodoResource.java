a5f3533ce2c2742ff99170ffeae8401d3a585de6
ildemaro-medina <ildemaro-medina@desarrollo-3>
2016-03-31 10:43:17 -0430

    Uso de Funciones Lamba

--- src/main/java/com/ildeguaro/api/TodoResource.java
+++ src/main/java/com/ildeguaro/api/TodoResource.java
@@ -1,39 +1,32 @@
 package com.ildeguaro.api;
-import static spark.Spark.*;
+import spark.*;
 
 
 import com.google.gson.Gson;
 import com.ildeguaro.dominio.ToDo;
 import com.ildeguaro.jdbc.dao.ToDoDAO;
-import com.ildeguaro.util.UtilJson;
-import java.util.Map;
+import org.codehaus.jackson.map.ObjectMapper;
 
-import spark.Request;
-import spark.Response;
-import spark.Route;
 
 public class TodoResource {
 	
-    public static void main(String[] args) {			
-        port(5000);		
-        get("/api/todos", new Route() {
-                public Object handle(Request req, Response resp) throws Exception {
-                        resp.status(200);					   
-                   return new Gson().toJson(ToDoDAO.getAll());
-                }
+    public static void main(String[] args) {
+        
+        Spark.port(5000);
+        
+        Spark.get("/api/todos","application/json",(req, resp) -> {
+            resp.status(200);					   
+            return new Gson().toJson(ToDoDAO.getAll());
+               
         });		
-        post("/api/todos/save","application/json", new Route() {
-                public Object handle(Request req, Response resp) throws Exception {
-                        resp.status(200);                                              
-                        String task = UtilJson.mapFromBody(req).get("task");    
-                        ToDoDAO.saveOne(new ToDo(0,task,true));	   
-                   return new Gson().toJson("Exito true");
-                }
+        Spark.post("/api/todos/save","application/json",(req, resp) -> {
+            resp.status(200);        
+            ToDo todo = new ObjectMapper().readValue(req.body(), ToDo.class);
+            ToDoDAO.saveOne(todo);	   
+            return new Gson().toJson("exito:true");
+                
         });
     }
-    
-    
-
 }
 
 
